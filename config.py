56637990a5abf88278527f7b94834e9e3b044498
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-03-18 15:22:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,26 +5,26 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config:
     # Client Config              
-    API_ID = int(os.environ.get("API_ID", "")) # ⚠️ Required
-    API_HASH = os.environ.get("API_HASH", "") # ⚠️ Required       
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
+    API_ID = int(os.environ.get("API_ID", "23941369")) # ⚠️ Required
+    API_HASH = os.environ.get("API_HASH", "bbd37235e41a95d03bc144ea6bc7b2cd") # ⚠️ Required       
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6609713747:AAH7Ijmim8l-dzaw2fn_0eB8aZgikanVVzc") # ⚠️ Required
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
 
     # Database Config 
-    DATABASE_URI = os.environ.get("DATABASE_URI", "") # ⚠️ Required
-    DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority") # ⚠️ Required
+    DATABASE_NAME = os.environ.get("DATABASE_NAME", "Test")
 
     # other configs 
-    OWNER_ID = int(os.environ.get("OWNER_ID", "")) # ⚠️ Required
+    OWNER_ID = int(os.environ.get("OWNER_ID", "6805306768")) # ⚠️ Required
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "")) # ⚠️ Required
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "")) # ⚠️ Required
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1002093365834")) # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '') # ⚠️ Required
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
     USERDATA = os.environ.get('USERDATA', 'USER_DATA')
     BOT_UPTIME  = time.time()
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None 
-    BOT_USERNAME = os.environ.get('BOT_USERNAME', "") # No need if you don't want to auto forward when bot restarted
+    BOT_USERNAME = os.environ.get('BOT_USERNAME', "FZXTest2Bot") # No need if you don't want to auto forward when bot restarted
     SESSION = os.environ.get('SESSION', '') # ⚠️ Required 
 
     # Web Support 
